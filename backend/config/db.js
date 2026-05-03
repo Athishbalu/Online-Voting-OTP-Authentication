579c7267c099ba08b51a07188f5633b6be771ded
B.Athish <athisha660@gmail.com>
2026-05-03 14:55:28 +0530

    fix mongodb connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,11 +6,12 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       serverSelectionTimeoutMS: 60000,
       socketTimeoutMS: 60000,
+      tls: true,
+      tlsAllowInvalidCertificates: true,
     });
     console.log("MongoDB Connected ✅");
   } catch (error) {
     console.error("MongoDB Error ❌", error.message);
-    // Don't exit - let server keep running
   }
 };
 
